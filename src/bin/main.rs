141c313986dd9222965927002a316137359f7c5f
Andrew Couch <andrew@cockroachlabs.com>
2018-12-07 14:45:50 -0500

    parse + stringify

---
+++ src/bin/main.rs
@@ -0,0 +1,24 @@
+extern crate wayfinder;
+
+fn main() {
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() < 2 {
+        eprintln!("Usage: {} ROUTE_FILE", args[0]);
+    }
+
+    let path = &args[1];
+    let contents = std::fs::read_to_string(path).unwrap();
+
+    match wayfinder::parse::route_config(&contents) {
+        Ok(config) => print!("{}", config.1.stringify()),
+        result => {
+            println!("cargo:warning=Template parse error in {:?}:", path);
+            wayfinder::parse::errors::show_errors(
+                &mut std::io::stdout(),
+                &contents,
+                result,
+                "cargo:warning=",
+            );
+        },
+    }
+}
