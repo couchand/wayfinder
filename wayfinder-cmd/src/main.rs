4e053adc5ab056c063faeec84641ce85716cc385
Andrew Dona-Couch <hi@andrewcou.ch>
2019-11-12 15:12:33 -0500

    fixup cmd

---
+++ wayfinder-cmd/src/main.rs
@@ -0,0 +1,13 @@
+use wayfinder_build::build;
+
+fn main() {
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() < 2 {
+        eprintln!("Usage: {} ROUTE_FILE", args[0]);
+        return;
+    }
+
+    let path = &args[1];
+
+    build(&path, &mut std::io::stdout());
+}
